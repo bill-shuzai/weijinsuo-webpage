2c6e62f9be9175a4f478b15e7f6fe9a510999981
zachui <475684606@qq.com>
2019-12-06 21:18:01 +0800

    update

--- js/main.js
+++ js/main.js
@@ -22,8 +22,37 @@ $(function(){
 			}
 
 		});
-	}
+	};
+
 
 	$(window).on('resize',resize).trigger('resize');
+
+	$('[data-toggle="tooltip"]').tooltip();
+
+	// 控制标签页的宽度
+	var $ulContainer=$('.nav-tabs');
+
+	var sumWidth=30;
+
+
+	$ulContainer.children().each(function(index,item){
+		sumWidth+=item.clientWidth;
+	});
+
+
+	/*判断当前ul的宽度是否超出屏幕*/
+	if(sumWidth > $(window).width()){
+		$ulContainer.css('width',sumWidth);
+		$('.ul-wapper').css('overflow-x','scroll');
+	}
+
+	var $news_title=$('.news-title');
+	
+
+	var $news_li=$('#news .nav.nav-stacked li > a' );
+	$news_li.on('click',function(){
+		var title=$(this).data('title');
+		$news_title.text(title);
+	});
 
 });
